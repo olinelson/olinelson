f5561be06f105c4d49994583875420b97501a01d
Oli Nelson <oliver.nelson@nirovision.com>
2023-03-06 22:43:43 +1100

    first commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<div>
+    <img src="./headshot.jpeg" alt="Oli Nelson" />
+    <hgroup>
+        <h2>Oli Nelson</h2>
+        <h3>Software Engineer | Bitcoin Maximalist | Drummer</h3>
+    </hgroup>
+</div>
+
+<style>
+    div {
+        display: grid;
+        place-items: center;
+    }
+    img {
+        border-radius: 100%;
+        width: 10rem;
+    }
+</style>
