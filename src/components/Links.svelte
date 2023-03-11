8f3795013f17e310f386c7dfb05964f674d69429
Oli Nelson <oliver.nelson@nirovision.com>
2023-03-11 17:36:41 +1100

    Update resume

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <section>
+  <div />
   <a
     href="https://snort.social/p/npub1rj70rm4vggrpcpvaukg2jpzp7qx9v9pfw5rmy9dth5fcn74y508qfekjvl"
     >Nostr</a
@@ -18,11 +19,15 @@
   <a href="https://twitter.com/o_j_nelson" target="_blank" rel="noreferrer"
     ><LogoTwitter size={32} /></a
   >
+  <a href="/resume.pdf" download target="_blank" rel="noreferrer">Resume</a>
+  <div />
 </section>
 
 <style>
   section {
-    text-align: center;
-    word-spacing: 1rem;
+    display: grid;
+    grid-template-columns: 1fr repeat(5, auto) 1fr;
+    grid-gap: 1rem;
+    place-items: center;
   }
 </style>
