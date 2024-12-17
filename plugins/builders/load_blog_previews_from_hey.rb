957603760fe5be088b926cec1ee98f2925968e15
olinelson <olivernelson@icloud.com>
2024-12-18 07:01:10 +1100

    touch up home page

--- plugins/builders/load_blog_previews_from_hey.rb
+++ plugins/builders/load_blog_previews_from_hey.rb
@@ -1,16 +1,18 @@
 require 'debug'
 require 'rss'
+require 'nokogiri'
 
 class Builders::LoadBlogPreviewsFromHey < SiteBuilder
   def build
     hook :site, :post_read do
     get "https://world.hey.com/olivernelson/feed.atom" do |data|
       feed = RSS::Parser.parse(data)
-      blog_post_previews = feed.items.map do |item|
+      blog_post_previews = feed.items.slice(0,3).map do |item|
         title = item.title.content
         published = item.published.content.to_s.split("T").at(0)
-        content = item.content.content
-        {title:, published:, content:}
+        content = Nokogiri::HTML(item.content.content).text
+        link = item.link.href
+        {title:, published:, preview: "#{content.slice(0,100)}...", link: }
       end
 
       site.data[:blog_post_previews] = blog_post_previews
