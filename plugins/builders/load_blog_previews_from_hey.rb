bedd04f5dc53eacfc0683ec7f7005a73fb676e02
olinelson <olivernelson@icloud.com>
2024-12-29 11:10:59 +1100

    cleanup

--- plugins/builders/load_blog_previews_from_hey.rb
+++ plugins/builders/load_blog_previews_from_hey.rb
@@ -5,18 +5,18 @@ require 'nokogiri'
 class Builders::LoadBlogPreviewsFromHey < SiteBuilder
   def build
     hook :site, :post_read do
-    get "https://world.hey.com/olivernelson/feed.atom" do |data|
-      feed = RSS::Parser.parse(data)
-      blog_post_previews = feed.items.slice(0,3).map do |item|
-        title = item.title.content
-        published = DateTime.parse(item.published.content.to_s).strftime("%B %d, %Y")
-        content = Nokogiri::HTML(item.content.content).text
-        link = item.link.href
-        {title:, published:, preview: "#{content.slice(0,100)}...", link: }
+      get "https://world.hey.com/olivernelson/feed.atom" do |data|
+        feed = RSS::Parser.parse(data)
+        blog_post_previews = feed.items.slice(0,3).map do |item|
+          title = item.title.content
+          published = item.published.content.strftime("%B %d, %Y")
+          content = Nokogiri::HTML(item.content.content).text
+          link = item.link.href
+          preview = "#{content.slice(0,100)}..."
+          {title:, published:, preview:, link: }
+        end
+        site.data[:blog_post_previews] = blog_post_previews
       end
-
-      site.data[:blog_post_previews] = blog_post_previews
-    end
     end
   end
 end
